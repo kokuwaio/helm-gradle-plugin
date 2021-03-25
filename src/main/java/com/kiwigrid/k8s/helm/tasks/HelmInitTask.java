fde80f09613658c999d13c91826847d525f0da63
Eugene <eugen.rabii@gmail.com>
2021-03-24 21:45:26 -0400

    minor changes

--- src/main/java/com/kiwigrid/k8s/helm/tasks/HelmInitTask.java
+++ src/main/java/com/kiwigrid/k8s/helm/tasks/HelmInitTask.java
@@ -1,10 +1,11 @@
 package com.kiwigrid.k8s.helm.tasks;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import com.kiwigrid.k8s.helm.HelmPlugin;
-import org.gradle.api.Task;
-import org.gradle.api.specs.Spec;
+import org.gradle.api.logging.Logger;
 import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.TaskAction;
 
@@ -15,13 +16,26 @@ import org.gradle.api.tasks.TaskAction;
  */
 public class HelmInitTask extends AbstractHelmTask {
 
+	private final Logger logger = getLogger();
+
 	public HelmInitTask() {
-		onlyIf(task -> !HelmPlugin.isVersion3OrNewer(getVersion()));
+		onlyIf(task -> {
+			String version = getVersion();
+			logger.debug("found version : " + version + " of helm");
+			boolean is30OrNewer = HelmPlugin.isVersion3OrNewer(version);
+			if(is30OrNewer) {
+				logger.debug("version : " + version + " is higher than 3.0. This operation is a NO-OP");
+			}
+			return !is30OrNewer;
+		});
 	}
 
 	@TaskAction
 	public void helmInit() {
-		HelmPlugin.helmExecSuccess(getProject(), this, "init", "--client-only");
+		String [] result = HelmPlugin.helmExecSuccess(getProject(), this, "init", "--client-only");
+		if(logger.isDebugEnabled()){
+			logger.debug("result of 'helm init --client-only' : \n" + String.join("\n", result));
+		}
 	}
 
 	@OutputDirectory
